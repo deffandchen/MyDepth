cd0a68acb6ae047772351342d2a7c18be0ef3886
deffandchen <defangch@163.com>
2020-12-26 18:43:14 +0800

    refactor variable

--- Model.py
+++ Model.py
@@ -124,43 +124,43 @@ class MyLoss(nn.Module):
         # STORE DISPARITIES
         #with tf.variable_scope('disparities'):
 
-        self.disp_est = [disp_list[0], disp_list[1], disp_list[2], disp_list[3]]
-        self.disp_left_est = [torch.unsqueeze(d[:, 0, :, :], 1) for d in self.disp_est]
-        self.disp_right_est = [torch.unsqueeze(d[:, 1, :, :], 1) for d in self.disp_est]
+        self.disp_pred = [disp_list[0], disp_list[1], disp_list[2], disp_list[3]]
+        self.disp_left_pred = [torch.unsqueeze(d[:, 0, :, :], 1) for d in self.disp_pred]
+        self.disp_right_pred = [torch.unsqueeze(d[:, 1, :, :], 1) for d in self.disp_pred]
 
         if self.mode == 'test':
             return
 
         # GENERATE IMAGES
         #with tf.variable_scope('images'):
-        self.left_est = [self.generate_image_left(self.right_pyramid[i], self.disp_left_est[i]) for i in range(4)]
-        self.right_est = [self.generate_image_right(self.left_pyramid[i], self.disp_right_est[i]) for i in range(4)]
+        self.left_pred = [self.generate_image_left(self.right_pyramid[i], self.disp_left_pred[i]) for i in range(4)]
+        self.right_pred = [self.generate_image_right(self.left_pyramid[i], self.disp_right_pred[i]) for i in range(4)]
 
         # LR CONSISTENCY
         #with tf.variable_scope('left-right'):
-        self.right_to_left_disp = [self.generate_image_left(self.disp_right_est[i], self.disp_left_est[i]) for i in
+        self.disp_right_to_left = [self.generate_image_left(self.disp_right_pred[i], self.disp_left_pred[i]) for i in
                                    range(4)]
-        self.left_to_right_disp = [self.generate_image_right(self.disp_left_est[i], self.disp_right_est[i]) for i in
+        self.disp_left_to_right = [self.generate_image_right(self.disp_left_pred[i], self.disp_right_pred[i]) for i in
                                    range(4)]
 
         # DISPARITY SMOOTHNESS
         #with tf.variable_scope('smoothness'):
-        self.disp_left_smoothness = self.get_disparity_smoothness(self.disp_left_est, self.left_pyramid)
-        self.disp_right_smoothness = self.get_disparity_smoothness(self.disp_right_est, self.right_pyramid)
+        self.disp_left_smoothness = self.get_disparity_smoothness(self.disp_left_pred, self.left_pyramid)
+        self.disp_right_smoothness = self.get_disparity_smoothness(self.disp_right_pred, self.right_pyramid)
 
     def build_losses(self):
         #with tf.variable_scope('losses', reuse=self.reuse_variables):
         # IMAGE RECONSTRUCTION
         # L1
-        self.l1_left = [torch.abs(self.left_est[i] - self.left_pyramid[i]) for i in range(4)]
+        self.l1_left = [torch.abs(self.left_pred[i] - self.left_pyramid[i]) for i in range(4)]
         self.l1_reconstruction_loss_left = [torch.mean(l) for l in self.l1_left]
-        self.l1_right = [torch.abs(self.right_est[i] - self.right_pyramid[i]) for i in range(4)]
+        self.l1_right = [torch.abs(self.right_pred[i] - self.right_pyramid[i]) for i in range(4)]
         self.l1_reconstruction_loss_right = [torch.mean(l) for l in self.l1_right]
 
         # SSIM
-        self.ssim_left = [self.ssim(self.left_est[i], self.left_pyramid[i]).mean(1,True) for i in range(4)]
+        self.ssim_left = [self.ssim(self.left_pred[i], self.left_pyramid[i]).mean(1, True) for i in range(4)]
         self.ssim_loss_left = [torch.mean(s) for s in self.ssim_left]
-        self.ssim_right = [self.ssim(self.right_est[i], self.right_pyramid[i]).mean(1,True) for i in range(4)]
+        self.ssim_right = [self.ssim(self.right_pred[i], self.right_pyramid[i]).mean(1, True) for i in range(4)]
         self.ssim_loss_right = [torch.mean(s) for s in self.ssim_right]
 
         # WEIGTHED SUM
@@ -178,9 +178,9 @@ class MyLoss(nn.Module):
         self.disp_gradient_loss = np.sum(self.disp_left_loss + self.disp_right_loss,axis=0)
 
         # LR CONSISTENCY
-        self.lr_left_loss = [torch.mean(torch.abs(self.right_to_left_disp[i] - self.disp_left_est[i])) for i in
+        self.lr_left_loss = [torch.mean(torch.abs(self.disp_right_to_left[i] - self.disp_left_pred[i])) for i in
                              range(4)]
-        self.lr_right_loss = [torch.mean(torch.abs(self.left_to_right_disp[i] - self.disp_right_est[i])) for i in
+        self.lr_right_loss = [torch.mean(torch.abs(self.disp_left_to_right[i] - self.disp_right_pred[i])) for i in
                               range(4)]
         self.lr_loss = np.sum(self.lr_left_loss + self.lr_right_loss,axis=0)
 
